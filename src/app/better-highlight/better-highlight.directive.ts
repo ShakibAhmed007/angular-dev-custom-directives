6fc352bfe06a78c4b30ad6d43c72e22c7283ad74
ShakibAhmed007 <shakibahmed09112017@gmail.com>
2021-03-28 13:29:29 +0600

    1 file modified

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -1,4 +1,10 @@
-import { Directive, ElementRef, OnInit, Renderer2 } from "@angular/core";
+import {
+  Directive,
+  ElementRef,
+  HostListener,
+  OnInit,
+  Renderer2
+} from "@angular/core";
 
 @Directive({
   selector: "[appBetterHighLight]"
@@ -6,10 +12,30 @@ import { Directive, ElementRef, OnInit, Renderer2 } from "@angular/core";
 export class BetterHighLightDirective implements OnInit {
   constructor(private renderer: Renderer2, private elementRef: ElementRef) {}
   ngOnInit() {
+    // this.renderer.setStyle(
+    //   this.elementRef.nativeElement,
+    //   "background-color",
+    //   "blue"
+    // );
+  }
+
+  /*
+  Using HostListener decorator to react on mouse enter and leave
+  */
+
+  @HostListener("mouseenter") mouseenter(eventData: Event) {
     this.renderer.setStyle(
       this.elementRef.nativeElement,
       "background-color",
       "blue"
+    );
+  }
+
+  @HostListener("mouseleave") mouseleave(eventData: Event) {
+    this.renderer.setStyle(
+      this.elementRef.nativeElement,
+      "background-color",
+      "transparent"
     );
   }
 }
